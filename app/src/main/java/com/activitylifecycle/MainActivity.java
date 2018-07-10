899eb247e668ecd8c17ce2cea9eeeebd37c94fed
Naresh1232 <33090603+Naresh1232@users.noreply.github.com>
2018-07-10 15:50:06 +0530

    Update MainActivity.java

--- app/src/main/java/com/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/activitylifecycle/MainActivity.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.microsoft.appcenter.AppCenter; 
+import com.microsoft.appcenter.analytics.Analytics; 
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
